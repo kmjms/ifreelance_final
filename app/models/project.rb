9eb8aceb3e58c299b5189bfa7f9d72cc27d4709b
juligarji <juliangarzon.j@gmail.com>
2017-10-25 10:47:50 -0500

    relaciones entre modelos de la bd

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,7 @@ class Project < ApplicationRecord
   belongs_to :freelance
   belongs_to :state
   belongs_to :type
+
+  has_many :items, dependent: :destroy
+  has_many :expenses, dependent: :destroy
 end
