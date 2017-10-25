c443a55244296cc51944a4ed158670e3e7db2b80
juligarji <juliangarzon.j@gmail.com>
2017-10-25 12:31:38 -0500

    cambios a controllador projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -26,6 +26,7 @@ class ProjectsController < ApplicationController
   # POST /projects.json
   def create
     @project = Project.new(project_params)
+    @clients = Clients.
 
     respond_to do |format|
       if @project.save
