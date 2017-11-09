0b7d3ff87dd82f7d5528dbfcf2dfae6f9e87bfc9
juligarji <jgarzonj@unal.edu.co>
2017-11-09 19:59:45 +0000

    sacando queries de los controladores

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,30 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def calculate_all_incomes
-    projects = current_freelance.projects
-    total_income = 0
-
-    projects.each do |proj|
-      total_income += proj.items.sum(:price)
-    end
-
-    return total_income
-end
-
-def calculate_all_expenses
-  projects = current_freelance.projects
-    
-  total_expenses = 0
-
-  projects.each do |proj|
-    total_expenses += proj.expenses.sum(:price)
-  end
-
-  return total_expenses
-end
-
-
 def not_found
   raise ActionController::RoutingError.new('Not Found')
 end
