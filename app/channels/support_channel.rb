9d207d249d4386094e2acc457578f478d255b2c0
juligarji <jgarzonj@unal.edu.co>
2017-11-16 20:12:36 +0000

    llamada a la creacion de mensajes

--- app/channels/support_channel.rb
+++ app/channels/support_channel.rb
@@ -1,14 +1,14 @@
 class SupportChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from "support_#{params[:id]}"
- end
+      def subscribed
+        stream_from "support_#{params[:id]}"
+     end
 
- def unsubscribed
-   # Any cleanup needed when channel is unsubscribed
- end
+     def unsubscribed
+       # Any cleanup needed when channel is unsubscribed
+     end
 
- def speak(message)
-   Support.find(params[:id]).messages.create(body:message['body'],sender_type:message['sender'].to_s)
- end
+     def speak(message)
+       Support.find(params[:id]).messages.create(body:message['body'],sender_type:message['sender'].to_s)
+     end
 
 end
